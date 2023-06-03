a9540a798856f8df52358616813c3aaf2ea8178d
k-s-b <karandeepbrar@gmail.com>
2023-06-03 18:16:24 +0900

    update paths

--- VIME/run_VIME.py
+++ VIME/run_VIME.py
@@ -29,13 +29,13 @@ print(args)
 chosen_data = args.data
 log_name = "%sData" % chosen_data[-1]
 if chosen_data == 'real-n':
-    data = dataset.Ndata(path='~/Custom-Semi-Supervised/data/ndata.csv')
+    data = dataset.Ndata(path='/home/singh/wco/data/ndata.csv')
 elif chosen_data == 'real-m':
-    data = dataset.Mdata(path='~/Custom-Semi-Supervised/data/mdata.csv')
+    data = dataset.Mdata(path='/home/singh/wco/data/mdata.csv')
 elif chosen_data == 'real-t':
-    data = dataset.Tdata(path='~/Custom-Semi-Supervised/data/tdata.csv')
+    data = dataset.Tdata(path='/home/singh/wco/data/tdata.csv')
 elif chosen_data == 'real-c':
-    data = dataset.Cdata(path='~/Custom-Semi-Supervised/data/cdata.csv')
+    data = dataset.Cdata(path='/home/singh/wco/data/cdata.csv')
 
 # args
 seed = args.seed
